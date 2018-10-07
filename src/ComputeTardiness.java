c74fcb1d49c9d95c9e17a7716efa80323f9e80bd
Niels Bakker <ncb21992@hotmail.com>
2018-10-07 15:52:25 +0200

    added the zip

--- src/ComputeTardiness.java
+++ src/ComputeTardiness.java
@@ -2,11 +2,11 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ComputeTardiness {	
+public class AdvancedAlgo {
 	public static ProblemInstance readInstance(String filename){
-		System.out.println(" ------- ");
-		System.out.println(" - Filename : " + filename);
-		System.out.println(" ------- \n");
+//		System.out.println(" ------- ");
+//		System.out.println(" - Filename : " + filename);
+//		System.out.println(" ------- \n");
 		ProblemInstance instance = null;
 		
 		try {
@@ -36,8 +36,8 @@ public class ComputeTardiness {
 	}
 
 	public static void writeResultsForTestData(){
-		String data_file_directory="/home/niels/projects/AdvancedAlgo/test-set/lessInstances/";
-		String output_path="/home/niels/projects/AdvancedAlgo/outputMyAlgo.csv";
+		String data_file_directory="/home/niels/projects/AdvancedAlgo/test-set/evenLessInstances/";
+		String output_path="/home/niels/projects/AdvancedAlgo/outputBestFirst.csv";
 		// create a file that is really a directory
 		File aDirectory = new File(data_file_directory);
 
@@ -52,27 +52,19 @@ public class ComputeTardiness {
 //			Greedy greedy = new Greedy(instance);
 //			Schedule greedySchedule = greedy.getSchedule();
 //			long runtimeGreedy     = System.currentTimeMillis()-startGreedy;
-//			long startBestFirst   = System.currentTimeMillis();
+//			result += String.format("%s;%s;%s;\n", file, greedySchedule.getTardiness(), runtimeGreedy);
+			long startBestFirst   = System.currentTimeMillis();
 //            int bestFirstTardiness = -1; //-1 implies error
-//			try {
-//				BestFirst bestFirst = new BestFirst(instance);
-//				Schedule bestFirstSchedule = bestFirst.getSchedule();
-////				System.out.println(bestFirstSchedule.getTardiness());
-//                bestFirstTardiness = bestFirstSchedule.getTardiness();
-//			}
-//			catch(OutOfMemoryError e){
-////				System.out.println("best first oom error");
-//			}
-////			int bestFirstTardiness = bestFirstSchedule==null? -1, bestFirstSchedule.getTardiness();
-//			long runtimeBestFirst = System.currentTimeMillis()-startBestFirst;
-////			System.out.println("best first runtime: "+(System.currentTimeMillis()-startBestFirst));
-////			start = System.currentTimeMillis();
+			try {
+				BestFirst bestFirst = new BestFirst(instance);
+				Schedule bestFirstSchedule = bestFirst.getSchedule();
 
-			long startMyFunc = System.currentTimeMillis();
-			MyAlgo myfunc = new MyAlgo(instance);
-			OurSchedule myFuncSchedule = myfunc.getSchedule();
-			long runtimeMyFunc = System.currentTimeMillis()-startMyFunc;
-			result += String.format("%s;%s;%s;\n", file, myFuncSchedule.getTardiness(0), runtimeMyFunc);
+                long runtimeBestFirst = System.currentTimeMillis()-startBestFirst;
+                result += String.format("%s;%s;%s;\n", file, bestFirstSchedule.getTardiness(), runtimeBestFirst);
+			}
+			catch(OutOfMemoryError e){
+                result += String.format("%s;%s;%s;\n", file, -1, -1);
+			}
 		}
 
         try(PrintWriter out = new PrintWriter(output_path)){
@@ -93,6 +85,7 @@ public class ComputeTardiness {
 //		Greedy greedy = new Greedy(instance);
 //		Schedule greedySchedule = greedy.getSchedule();
 //		long end1     = System.currentTimeMillis()-start1;
+//        long start = System.currentTimeMillis();
 //		try {
 //			BestFirst bestFirst = new BestFirst(instance);
 //			Schedule bestFirstSchedule = bestFirst.getSchedule();
@@ -102,14 +95,15 @@ public class ComputeTardiness {
 //			System.out.println("best first oom error");
 //		}
 //        System.out.println("best first runtime: "+(System.currentTimeMillis()-start));
-//        start = System.currentTimeMillis();
 
-		long start = System.currentTimeMillis();
+
+//		long start = System.currentTimeMillis();
 		MyAlgo myfunc = new MyAlgo(instance);
 		OurSchedule myFuncSchedule = myfunc.getSchedule();
-		System.out.println("\n -------------------------------- ");
-		System.out.println("1. myAlgo min.Tard : " + Integer.toString(myFuncSchedule.getTardiness(0)));
-        System.out.println("1. myAlgo runtime : " + (System.currentTimeMillis()-start));
+		System.out.print(myFuncSchedule.getTardiness(0));
+//		System.out.println("\n -------------------------------- ");
+//		System.out.println("1. myAlgo min.Tard : " + Integer.toString(myFuncSchedule.getTardiness(0)));
+//        System.out.println("1. myAlgo runtime : " + (System.currentTimeMillis()-start));
 
 //		System.out.println("\n -------------------------------- ");
 //		System.out.println("1. Greedy min.Tard : " + Integer.toString(greedySchedule.getTardiness()));
